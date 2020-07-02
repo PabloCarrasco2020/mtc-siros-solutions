581b3e55fba45e79d662a9c471044b79ee4851a1
Alex Fernando Torres Chahuara <superahacker@gmail.com>
2020-07-02 14:40:42 -0500

    FiX NAMES

--- Application.Interface/IBaseApplication.cs
+++ Application.Interface/IBaseApplication.cs
@@ -6,11 +6,11 @@ using Transversal.Common;
 
 namespace Application.Interface
 {
-    public interface IBaseApplication<RQInsert,RQUpdate,RSGET>
+    public interface IBaseApplication<T_RQInsert,T_RQUpdate,T_RSGET>
     {
-        Task<Response<int>> Insert(RQInsert input);
-        Task<Response<int>> Update(RQUpdate input);
-        Task<Response<RSGET>> Get(string input);
+        Task<Response<int>> Insert(T_RQInsert input);
+        Task<Response<int>> Update(T_RQUpdate input);
+        Task<Response<T_RSGET>> Get(string input);
         Task<Response<IndexTableModelDto>> GetAllByFilter(int pagina, string filter);
         Task<Response<List<ComboModelDto>>> GetCombo(string input);
     }
