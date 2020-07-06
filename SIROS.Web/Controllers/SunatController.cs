5ec54518e54499e13ecfc5e121f9e1a30ea5b028
Aaron Cisneros <aaron.joffre.ke@gmail.com>
2020-07-06 11:12:22 -0500

    servicio sunat consulta ruc

---
+++ SIROS.Web/Controllers/SunatController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Transversal.Common;
+
+namespace SIROS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SunatController : ControllerBase
+    {
+        [HttpGet("ConsultaRuc")]
+        public async Task<IActionResult> ConsultaRuc(string sRuc)
+        {
+            try
+            {
+                var wsSunat = new wsSoapSUNAT.DatosRucWSFacadeRemoteClient();
+                var oResult = await wsSunat.getDatosPrincipalesAsync(sRuc);
+                return Ok(oResult);
+            }
+            catch (Exception ex)
+            {
+                // Log
+                return Ok(new Response<Object> { Message = "[SUNAT]: ERR-Fallo en el servidor" });
+            }
+        }
+    }
+}
