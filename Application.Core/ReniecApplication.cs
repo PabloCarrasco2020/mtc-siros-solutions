b5d5b6715a6a51b6d30511f3099a08810a75ccdf
Aaron Cisneros <aaron.joffre.ke@gmail.com>
2020-07-07 13:10:32 -0500

    reniec service

---
+++ Application.Core/ReniecApplication.cs
@@ -0,0 +1,74 @@
+﻿using Application.Dto;
+using Application.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Transversal.Common;
+
+namespace Application.Core
+{
+    public class ReniecApplication : IReniecApplication
+    {
+        private readonly IConfiguration _configuration;
+        private string _sReniecUrl;
+
+        public ReniecApplication(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._sReniecUrl = this._configuration.GetSection("Servicios").GetSection("ReniecAPI").Value + "Reniec/{0}";
+        }
+
+        public async Task<Response<ReniecDto.ConsultaNumDocResponseModel>> ConsultaNumDoc(string sNumDoc)
+        {
+            try
+            {
+                var oResponse = new Response<ReniecDto.ConsultaNumDocResponseModel>();
+                oResponse.IsSuccess = false;
+
+                using (WebApiClient oHttpClient = new WebApiClient())
+                {
+                    string sUrl = string.Format(this._sReniecUrl, sNumDoc);
+                    var oClientResponse = await oHttpClient.CallGetAsync<ReniecDto.ReniecResult>(sUrl);
+                    if (oClientResponse.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (oClientResponse.Content != null)
+                        {
+                            if (oClientResponse.Content.result != null)
+                            {
+                                var oResult = new ReniecDto.ConsultaNumDocResponseModel();
+                                oResult.nPersonId = oClientResponse.Content.result.personId;
+                                oResult.sNumDoc = oClientResponse.Content.result.dni;
+                                oResult.sApellidoPaterno = oClientResponse.Content.result.lastNameDad;
+                                oResult.sApellidoMaterno = oClientResponse.Content.result.lastNameMom;
+                                oResult.sNombres = oClientResponse.Content.result.name;
+                                oResult.sDireccion = oClientResponse.Content.result.address;
+                                oResult.sEstado = oClientResponse.Content.result.status;
+                                oResult.sFoto = oClientResponse.Content.result.photo;
+
+                                oResponse.IsSuccess = true;
+                                oResponse.Data = oResult;
+                            }
+                        }
+                    }
+                    else if ((int)oClientResponse.StatusCode == 422) //es menor de edad o no se encuentra
+                    {
+                        oResponse.Message = "(RENIEC) Error, La persona es un menor de edad o el numero de documento no existe;";
+                    }
+                    else
+                    {
+                        oResponse.Message = "(RENIEC) Error al realizar la consulta;";
+                    }
+                }
+
+                return oResponse;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Application.Dto/ReniecDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto
+{
+    public class ReniecDto
+    {
+        #region Response Models
+
+        public class ConsultaNumDocResponseModel
+        {
+            public int nPersonId { get; set; }
+            public string sNumDoc { get; set; }
+            public string sApellidoPaterno { get; set; }
+            public string sApellidoMaterno { get; set; }
+            public string sNombres { get; set; }
+            public string sDireccion { get; set; }
+            public string sEstado { get; set; }
+            public string sFoto { get; set; }
+        }
+
+        #endregion
+
+        public class ReniecResult
+        {
+            public ReniecPerson result { get; set; }
+            public float delay { get; set; }
+            public string origin { get; set; }
+        }
+
+        public class ReniecPerson
+        {
+            public int personId { get; set; }
+            public string dni { get; set; }
+            public string lastNameDad { get; set; }
+            public string lastNameMom { get; set; }
+            public string name { get; set; }
+            public string address { get; set; }
+            public string status { get; set; }
+            public string photo { get; set; }
+            public DateTime inserted { get; set; }
+        }
+    }
+}
