9dfdd6925177d853a2085b107012362f24456614
Gustavo Marcos GARCIA MANTARI <a1000145@idat.edu.pe>
2020-07-13 18:40:04 -0500

    Levantamiento de Observaciones

--- SIROS.Web/ClientApp/src/app/pages/municipalidad/municipalidad.component.ts
+++ SIROS.Web/ClientApp/src/app/pages/municipalidad/municipalidad.component.ts
@@ -648,4 +648,16 @@ export class MunicipalidadComponent implements OnInit {
     }
     return null;
   }
+  KeyUpNumber(event: any) {
+    const code: string = event.code;
+    if (code.startsWith('Key') || code.startsWith('Space')) {
+      event.preventDefault();
+    }
+  }
+  KeyUpLetter(event: any) {
+    const code: string = event.code;
+    if (code.startsWith('Digit')) {
+      event.preventDefault();
+    }
+  }
 }
